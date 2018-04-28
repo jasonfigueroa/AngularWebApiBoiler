9892839d0ccc34956aa9918c4ad86dde1f74a8c4
Jason Figueroa <jason.figueroa2@gmail.com>
2018-04-27 21:01:37 -0500

    initial commit

---
+++ AngularWebApiBoiler/Controllers/AppController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AngularWebApiBoiler.Controllers
+{
+    public class AppController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet("/contact")]
+        public IActionResult Contact()
+        {
+            ViewBag.Title = "Contact Us";
+
+            throw new InvalidOperationException("Bad things happened");
+
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewBag.Title = "About Us";
+
+            return View();
+        }
+    }
+}
---
+++ AngularWebApiBoiler/wwwroot/js/index.js
@@ -0,0 +1,21 @@
+﻿$(document).ready(function () {
+    var theForm = $('#the-form');
+    theForm.hide();
+
+    var button = $('#buy-button');
+    button.on('click', function () {
+        console.log("Buying Item");
+    });
+
+    var productInfo = $('.product-props li');
+    productInfo.on('click', function () {
+        console.log("You clicked on " + $(this).text());
+    });
+
+    var $loginToggle = $('#login-toggle');
+    var $popupForm = $('.popup-form');
+
+    $loginToggle.on('click', function () {
+        $popupForm.fadeToggle(500);
+    });
+});
